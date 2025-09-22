24c936cc08e8efbd85e60a19c065b820025bd3b2
“erwrervv” <erwrervv@gmail.com>
2025-09-22 10:57:10 +0800

    9commit 添加了launch.json

---
+++ lesson2/lesson2_1.py
@@ -0,0 +1,15 @@
+def calculate_sum(a, b):
+    result = a + b
+    return result
+
+def main():
+    number1 = 10
+    number2 = 20
+    total = calculate_sum(number1, number2)
+    print(f"計算結果: {total}")
+
+print("hel")
+if __name__ == "__main__":
+    print("這是python直接執行")
+else:
+    print("這不是python直接執行")
