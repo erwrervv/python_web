7debb5842ee7a9394d3d913693825a966a88e34f
“erwrervv” <erwrervv@gmail.com>
2025-09-25 16:03:19 +0800

    23 commit增加了地區查詢篩選

--- lesson3/lesson3_3.py
+++ lesson3/lesson3_3.py
@@ -1,12 +1,12 @@
 import tools
 def main():
     ##希望在main這裡顯示錯誤
-   try:
-    data = tools.download_youbike_data()
-    print(data)
-   except Exception as e:
-       print("發生錯誤\n{e}")
-
+    try:
+        data = tools.download_youbike_data()
+        areas = tools.get_area(data)
+        print(areas)
+    except Exception as e:
+        print("發生錯誤\n{e}")
 if __name__ == "__main__":
     main()
 # def download_youbike_data()->list:
